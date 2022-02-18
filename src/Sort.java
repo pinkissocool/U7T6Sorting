7e0439fd6e6d831e686d0edbea0da9191b7b82aa
msteenhuis <moizs@nycstudents.net>
2022-02-18 10:56:02 -0500

    stuff

--- src/Sort.java
+++ src/Sort.java
@@ -28,32 +28,62 @@ public class Sort
   // Part A.  Which Sort is Which?
   public static void insertionSort(int[] elements)
   {
-    int count = 0;
+    
     for (int j = 1; j < elements.length; j++)
     {
       int temp = elements[j];
       int possibleIndex = j;
-      while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
+      while (possibleIndex > 0 && temp > elements[possibleIndex - 1])
       {     
         elements[possibleIndex] = elements[possibleIndex - 1];
         possibleIndex--;
-        count++;
+
       }
       elements[possibleIndex] = temp;
 
     }
-    System.out.println("Insert: " + count);
+    System.out.println("Insert: ");
   }
   
   // Part C.  Sorting a 1000-word list!
   public static void selectionSortWordList(ArrayList<String> words)
   {
-    /* TO BE IMPLEMENTED IN PART C */
+
+    for (int j = 0; j < words.size() - 1; j++)
+    {
+      int minIndex = j;
+      for (int k = j + 1; k < words.size(); k++)
+      {
+        if (words.get(k).compareTo(words.get(minIndex)) < 0)
+        {
+          minIndex = k;
+        }
+
+      }
+      String temp = words.get(j);
+      words.set(j, words.get(minIndex));
+      words.set(minIndex, temp);
+
+    }
+
   }
 
   // Part C.  Sorting a 1000-word list!
-  public static void insertionSortWordList(ArrayList<String> words)
-  {    
-    /* TO BE IMPLEMENTED IN PART C */
+  public static void insertionSortWordList(ArrayList<String> elements)
+  {
+    for (int i = 1; i < elements.size(); i++)
+    {
+      String temp = elements.get(i);
+      int possibleIndex = i;
+      while (possibleIndex > 0 && temp.compareTo(elements.get(possibleIndex-1)) < 0)
+      {
+        elements.set(possibleIndex, elements.get(possibleIndex-1));
+        possibleIndex--;
+
+      }
+      elements.set(possibleIndex, temp);
+
+    }
+
   }
 }
